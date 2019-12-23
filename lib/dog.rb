0d6b87f15f1f0fb3351d5c6b5c891c74048473e0
George Blackmore <gtblackmore@gmail.com>
2019-12-23 03:58:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ attr_accessor :name
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def self.all
@@ -19,7 +19,9 @@ attr_accessor :name
   def self.print_all
     puts @@all.each{|dog| dog.name}
   end
-      
+  
+  def save
+    @@all << self
     
   
 end
